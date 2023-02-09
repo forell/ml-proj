7ae8fe7fdbbd1541b1f2b99af1244b4524d8c02a
Patrycja Balik <root@pbalik.com>
2023-02-10 00:03:05 +0100

    Read weather data into numpy arrays

--- model_with_dicts.py
+++ model_with_dicts.py
@@ -5,38 +5,83 @@ import geopy.distance as geo
 import numpy as np
 import pandas as pd
 
-"""The columns used as input for the prediction."""
+"""
+The columns used as input for the prediction.
+
+Available columns:
+TMAX — max temperature              [°C]
+TMIN — min temperature              [°C]
+STD  — mean temperature             [°C]
+TMNG — min ground-level temperature [°C]
+SMDB — total precipitation          [mm]
+opad — type of precipation          [0 — rain, 1 — snow]
+PKSN — snow cover height            [cm]
+RWSN — snow water equivalent        [mm/cm]
+USL  — sunshine duration            [hours]
+DESZ — rainfall duration            [hours]
+SNEG — snowfall duration            [hours]
+DISN — mixed rain and snow duration [hours]
+GRAD — hail duration                [hours]
+MGLA — fog duration                 [hours]
+ZMGL — mist duration                [hours]
+SADZ — rime ice duration            [hours]
+GOLO — black ice duration           [hours]
+ZMNI — drifting snow duration       [hours]
+ZMWS — ground blizzard duration     [hours]
+ZMET — haze duration                [hours]
+FF10 — wind speed ≥ 10 m/s duration [hours]
+FF15 — wind speed > 15 m/s duration [hours]
+BRZA — thunderstorm duration        [hours]
+ROSA — dew duration                 [hours]
+SZRO — frost duration               [hours]
+DZPS — snow cover                   [0/1]
+DZBL — lightning                    [0/1]
+grun — ground frozen                [0/1]
+IZD
+IZG
+AKTN
+"""
 weather_labels = ['TMAX', 'TMIN', 'STD', 'TMNG', 'SMDB', 'opad', 'PKSN',
                   'RWSN', 'USL', 'DESZ', 'SNEG', 'DISN', 'GRAD', 'MGLA',
                   'ZMGL', 'SADZ', 'GOLO', 'ZMNI', 'ZMWS', 'ZMET', 'FF10',
                   'FF15', 'BRZA', 'ROSA', 'SZRO', 'DZPS', 'DZBL', 'grun',
                   'IZD', 'IZG', 'AKTN']
 
-# num_of_weather_attr = len(weather_labels) # ??
-num_of_weather_attr = 2
+
+num_of_weather_attr = len(weather_labels)
+
 
 def load_data(weather_path, delay_path):
-    """
-    TODO: real weather!!!
-   
-    """        
-    # dumb weather for test (5 weather stations; 3 dates (only these in dumb_delays.csv), 2 parameters of wether)
-    station_coors = [(52.8758646, 18.6963912), (49.8557919, 19.3533583), (49.858467, 19.323956), (49.3406059, 20.8209928), (53.1291537, 17.4865518)]
+    def weather_to_numpy(coords, data):
+        data = [ list(data[coord].values()) for coord in coords ]
+        return np.array(data).T
+
     weather = dict()
-    weather["2019-08-31"] = np.arange(10).reshape(2, 5)
-    weather["2019-09-01"] = -np.arange(10).reshape(2, 5)
-    weather["2019-09-02"] = np.arange(10).reshape(2, 5)**2
+    coords = set()
+
+    with open(weather_path, 'r') as weather_data:
+        for l in weather_data:
+            measurement = json.loads(l)
+            date = measurement['date']
+            lat = measurement['lat']
+            lon = measurement['lon']
+            measurement = { k : measurement[k] for k in weather_labels }
+            weather.setdefault(date, {})[(lat, lon)] = measurement
+            coords.add((lat, lon))
+
+    coords = list(coords)
+    weather = { k : weather_to_numpy(coords, v) for k, v in weather.items() }
 
     with open(delay_path, 'r') as delay_data:
-        delays = [row for row in csv.reader(delay_data)]
+        delays = list(csv.reader(delay_data))
 
-    return weather, station_coors, delays
+    return weather, coords, delays
 
     
-def calculate_coefficients(weather_stations_coors, lat, lon):
-    dists_list = [geo.distance(station_coor, (lat, lon)).km for station_coor in weather_stations_coors]
+def calculate_coefficients(weather_coords, lat, lon):
+    dists_list = [geo.distance(coord, (lat, lon)).km for coord in weather_coords]
     dists = np.array(dists_list)
-    weights = np.e ** -dists
+    weights = np.exp(-dists)
     return weights, np.sum(weights)
     
     
@@ -46,7 +91,7 @@ def weather_at(weather, date, coeffs_info):
     return np.sum(coeffs * day_weather, axis=-1)/coeffs_sum
 
 
-def combine_data(weather, weather_stations_coords, delays):
+def combine_data(weather, weather_coords, delays):
     X_weather = np.empty((0, num_of_weather_attr))
     Y_delays = np.empty((0, 1))
     
@@ -59,7 +104,7 @@ def combine_data(weather, weather_stations_coords, delays):
         lat, lon, date, delay = d
         if (lat, lon) not in weather_middlepoints:
             weather_middlepoints[(lat, lon)] = dict()
-            coeffs[(lat, lon)] = calculate_coefficients(weather_stations_coords, lat, lon)
+            coeffs[(lat, lon)] = calculate_coefficients(weather_coords, lat, lon)
         if date not in weather_middlepoints[(lat, lon)]:
             weather_middlepoints[(lat, lon)][date] = weather_at(weather, date, coeffs[(lat, lon)])
         row = weather_middlepoints[(lat, lon)][date].reshape(1, -1)
@@ -70,11 +115,9 @@ def combine_data(weather, weather_stations_coords, delays):
 
 
 def main():
-    weather, station_coords, delays = load_data('tu cokolwiek na razie', 'dumb_delays.csv')
-    X_weather, Y_delays = combine_data(weather, station_coords, delays)
+    weather, weather_coords, delays = load_data('data/weather_readings.json', 'data/minidelays.csv')
+    X, y = combine_data(weather, weather_coords, delays)
 
 
 if __name__ == '__main__':
     main()
-    
-    
--- resources.md
+++ resources.md
@@ -5,13 +5,20 @@ Datasets
 --------
 
 Weather:
-- [IMGW public data](https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/),
+- [IMGW public data](https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/).
+
+  **Additonal comments:**
+  - There is no data for Hala Gąsienicowa prior to 2019.
+  - The data for Lębork is incomplete in 2021.
+  - The Resko-Smólsko station is missing data between 2015 and 2018.
+
+  [Full information on data completeness on the IMGW website](https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/mapa_zawartosci_synop.pdf).
 - [US, Canada & Israel on Kaggle](https://www.kaggle.com/datasets/selfishgene/historical-hourly-weather-data).
 
 Public transport:
 - [InfoPasażer Archiver](http://ipa.lovethosetrains.com)
-  ([repo](https://github.com/tmaciejewski/ipa)),
-- [UTK public data](https://dane.utk.gov.pl/sts/punktualnosc/punktualnosc-pasazerska/19431,Punktualnosc-przewozow-pasazerskich.html),
+  ([repo](https://github.com/tmaciejewski/ipa)).
+- [UTK public data](https://dane.utk.gov.pl/sts/punktualnosc/punktualnosc-pasazerska/19431,Punktualnosc-przewozow-pasazerskich.html).
 - [OSM per-country data](https://download.geofabrik.de) contains information
   about train stations, among other things.
 
